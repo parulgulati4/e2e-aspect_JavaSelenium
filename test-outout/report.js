29f148eb1645bbf2fec0436dc8d19fa12e76c17d
Dorababu Chodavarapu <dorababuch.techmahindra@gmail.com>
2022-02-01 20:49:02 +0530

    first commit

--- test-outout/report.js
+++ test-outout/report.js
@@ -107,3 +107,32 @@ formatter.match({
   ],
   "location": "Login.user_will_open_the_page(String)"
 });
+formatter.result({
+  "duration": 285445059500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "name1",
+      "offset": 39
+    },
+    {
+      "val": "5",
+      "offset": 47
+    }
+  ],
+  "location": "Login.user_will_Enter_UserName_and_Password(String,String)"
+});
+formatter.result({
+  "duration": 318622900,
+  "error_message": "org.openqa.selenium.ElementNotInteractableException: element not interactable\n  (Session info: chrome\u003d97.0.4692.99)\nBuild info: version: \u00273.14.0\u0027, revision: \u0027aacccce0\u0027, time: \u00272018-08-02T20:19:58.91Z\u0027\nSystem info: host: \u0027LAPTOP-Q2CKDEE8\u0027, ip: \u0027192.168.56.1\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_301\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 97.0.4692.99, chrome: {chromedriverVersion: 97.0.4692.71 (adefa7837d02a..., userDataDir: C:\\Users\\dorab\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:64443}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 2d821092fae78110486aa6d76205f80a\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:548)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:276)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.sendKeys(RemoteWebElement.java:100)\r\n\tat com.stepdefinitions.Login.user_will_Enter_UserName_and_Password(Login.java:43)\r\n\tat ✽.Then User will Enter UserName and Password \"name1\" \"5\"(Login.feature:9)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "Login.user_will_Click_on_Login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
